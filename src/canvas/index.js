e991d0d5b7848f2012840e7dfbbde2d19868670e
crosshj <github@chimpjuice.com>
2017-04-15 03:14:27 -0400

    Update index.js

--- src/canvas/index.js
+++ src/canvas/index.js
@@ -1,4 +1,21 @@
 (()=> {
-  function ready(){}
+  var id, d;
+  function setCanvasPixel(context, {r=0, g=0, b=0, a=1}, {x=0, y=0}){
+    id = id || context.createImageData(1,1); // only do this once per page
+    d  = d || id.data;                        // only do this once per page
+    d[0]   = r;
+    d[1]   = g;
+    d[2]   = b;
+    d[3]   = a;
+    context.putImageData( id, x, y );  
+  }
+  
+  function ready(){
+    var canvas = document.getElementById('myCanvas');
+    var ctx = canvas.getContext('2d');
+    var setPixel = (color, pos) => setCanvasPixel(ctx, color, pos);
+    
+    setPixel({r:255}, {x:10, y:10});
+  }
   document.addEventListener('DOMContentLoaded', ready, false);
 })();
