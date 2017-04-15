0ccb1e9f30e7a8af6e63ad7fdaee4e01989f3079
crosshj <github@chimpjuice.com>
2017-04-15 03:42:56 -0400

    Update index.js

--- src/canvas/index.js
+++ src/canvas/index.js
@@ -14,14 +14,30 @@
     return new Array(to).fill().map((a,b)=>b);
   }
   
+  function randomInt(min, max) {
+    return Math.floor(max - Math.random()*(max-min))
+  }
+  
+  // TODO: better ways of doing this exist (not one pixel at a time)
+  function randomPixel(setter){
+    range(0, 640).forEach(x => {
+      range(0, 480).forEach(y => {
+        setter({
+          r: randomInt(0,255),
+          g: randomInt(0,255),
+          b: randomInt(0,255)
+        }, {x, y});
+      });
+    });
+  }
+  
   function ready(){
     var canvas = document.getElementById('myCanvas');
     var ctx = canvas.getContext('2d');
     // boo, bad global
     window.setPixel = (color, pos) => setCanvasPixel(ctx, color, pos);
     window.range = range;
-    
-    setPixel({r:255}, {x:10, y:10});
+    randomPixel(setPixel);
   }
   document.addEventListener('DOMContentLoaded', ready, false);
 })();
