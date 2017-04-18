137c3ef16c40823ed61deb60d3b1091a6e1cf8ea
crosshj <github@chimpjuice.com>
2017-04-17 23:54:12 -0400

    optimize and add ideas

--- src/canvas/index.js
+++ src/canvas/index.js
@@ -33,6 +33,24 @@
 //       document.getElementById("result").innerHTML = event.data;
 //   }
   
+  // dynamically append css
+  function addcss(css){
+    var head = document.getElementsByTagName('head')[0];
+    var s = document.createElement('style');
+    s.setAttribute('type', 'text/css');
+    if (s.styleSheet) {   // IE
+        s.styleSheet.cssText = css;
+    } else {                // the world
+        s.appendChild(document.createTextNode(css));
+    }
+    head.appendChild(s);
+  }
+  
+  // TODO: fully create canvas in DOM given root element and dimensions
+  // ...
+  
+  
+  // set one pixel at a time
   var id, d;
   function setCanvasPixel(context, {r=0, g=0, b=0, a=255}, {x=0, y=0}){
     id = id || context.createImageData(1,1); // only do this once per page
@@ -44,10 +62,12 @@
     context.putImageData( id, x, y );  
   }
   
+  // used for setting entire page at a time
   function newImageData(context, {x=1,y=1}){
     return context.createImageData(x, y);
   }
   
+  // used for setting entire page at a time
   var ad;
   function setImageDataPixel(imageData, {r=0, g=0, b=0, a=255}, {x=0, y=0, xmax=1}){
     ad = ad || imageData.data;
@@ -62,22 +82,25 @@
   }
   
   function range(from, to){
-    return new Array(to).fill().map((a,b)=>b);
+    return new Array(to).fill();
   }
   
   function randomInt(min, max) {
     return Math.floor(max - Math.random()*(max-min))
   }
   
-  // TODO: better ways of doing this exist (not one pixel at a time)
+  function randomRGB(){
+    var num = Math.round(0xffffff * Math.random());
+    var r = num >> 16;
+    var g = num >> 8 & 255;
+    var b = num & 255;
+    return { r, g, b };
+  }
+  
   function randomPixel(setter){
-    range(0, 640).forEach(x => {
-      range(0, 480).forEach(y => {
-        setter({
-          r: randomInt(0,255),
-          g: randomInt(0,255),
-          b: randomInt(0,255)
-        }, {x, y, xmax: 640});
+    range(0, 640).forEach((unused_x, x) => {
+      range(0, 480).forEach((unused_y, y) => {
+        setter(randomRGB(), {x, y, xmax: 640});
       });
     });
   }
