efc5caedc0c07b93d0fef8215027073655a7d296
HJ Cross <github@chimpjuice.com>
2017-05-03 00:00:37 -0400

    reandom to example

--- src/canvas/index.js
+++ src/canvas/index.js
@@ -1,16 +1,51 @@
 ((window)=> {
 
-  var options = {
-    // init: function(setter) {
-    //   var ctx = this.canvas.getContext('2d');
+  function range(from, to){
+    return new Array(to).fill();
+  }
 
-    //   requestAnimationFrame(
-    //     () => {
-    //       ctx.fillStyle = "#FF0000";
-    //       ctx.fillRect(0,0,10,10);
-    //     }
-    //   );
-    // }
+  // http://stackoverflow.com/a/23095731/1627873
+  function randomRGB(){
+    var num = Math.round(0xffffff * Math.random());
+    var r = num >> 16;
+    var g = num >> 8 & 255;
+    var b = num & 255;
+    return { r, g, b };
+  }
+
+  function randomPixel(setter, xmax, ymax){
+    range(0, xmax).forEach((unused_x, x) => {
+      range(0, ymax).forEach((unused_y, y) => {
+        var _color = randomRGB();
+        _color.r = _color.b = 0;
+        setter(_color, {x, y, xmax});
+      });
+    });
+  }
+
+  var randomImageData;
+  function randomPixels(setter){
+    if (!setter) return;
+
+    var ctx = this.canvas.getContext('2d');
+
+    randomImageData = randomImageData
+      ? randomImageData
+      : ctx.createImageData(this.dimensions.x, this.dimensions.y);
+
+    randomPixel(
+      (color, pos) => { setter(randomImageData, color, pos)},
+      this.dimensions.x,
+      this.dimensions.y
+    );
+
+    requestAnimationFrame(
+      () => ctx.putImageData( randomImageData, 0, 0 )
+    );
+  }
+
+  var options = {
+    init: randomPixels
   };
 
 
